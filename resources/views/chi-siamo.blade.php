21d9fe528187faed91bce11744ee4c23ceca2060
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-12 19:09:17 +0100

    Aggiunto link alla pagina Home che cliccato porta ad un'altra pagina "chi siamo"

--- resources/views/chi-siamo.blade.php
+++ resources/views/chi-siamo.blade.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First Step Laravel</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
 </head>
 <body>
-    <h1>Hello world!</h1>
-
-    <a href="chi-siamo">CHI SIAMO</a>
+    Chi siamo
 </body>
 </html>
